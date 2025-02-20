c3bb85ba8df547ff7756b7e3110b293cf39fdd7e
Vin <zyxio939@gmail.com>
2025-02-20 23:12:57 +0800

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h1 style={{ fontSize: '2.0rem', fontWeight: '650', marginBottom: '1rem' }}>
+        Static Quiz App
+      </h1>
+      <p style={{ fontSize: '1.5rem', fontWeight: '650' }}>Project name: quiz-app</p>
+    </div>
+  )
+}
+
+export default Header
